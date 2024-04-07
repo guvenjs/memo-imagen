7511ce11a483890f5c0fff4b0e82c6cf4b5d8a25
Enes Güven <enesguven09@gmail.com>
2024-04-07 13:14:32 +0300

    implementation fastlane

--- android/app/src/main/java/com/boilerplate/typescript/MainActivity.kt
+++ android/app/src/main/java/com/boilerplate/typescript/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.memo.imagen
+package com.memoimagen
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/boilerplate/typescript/MainApplication.kt
+++ android/app/src/main/java/com/boilerplate/typescript/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.memo.imagen
+package com.memoimagen
 
 import android.app.Application
 import com.facebook.react.PackageList
